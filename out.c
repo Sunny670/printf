f6b36e6fcdea6ef2dde7e346ce1cfcef42bd7e3c
Sunny670 <sunny.thanki13@gmail.com>
2023-01-25 10:51:57 -0800

    printf

---
+++ out.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _printf - Produes output according to a format
+ * @format: Character string
+ *
+ * Return: Number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i;
+
+	_printf("%c\n", H);
+	_printf("%s\n", String);
+	_printf("%%\n", %);
+
+	return (0);
+}
+
